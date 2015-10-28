9035e5655344e8c1d5e6b71537cb031809c978dc
franck2 <f.boncler@gmail.com>
2015-10-28 18:00:49 +0100

    initialisation of project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ribFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ribFrontendApp
+ */
+angular.module('ribFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
