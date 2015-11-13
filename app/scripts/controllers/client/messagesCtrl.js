345bbe2891e45df5ef68a7ffbb1110f676bb6128
franck2 <f.boncler@gmail.com>
2015-11-13 08:01:39 +0100

    interface message ok

--- app/scripts/controllers/client/messagesCtrl.js
+++ app/scripts/controllers/client/messagesCtrl.js
@@ -9,9 +9,31 @@
  */
 angular.module('ribFrontendApp')
   .controller('MessagesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	  
+	  this.messages = [
+	                   {
+	                	 author:"admin",
+	                	 date:2,
+	                	 message:"qsdfh f sqldf q"
+	                   },
+	                   {
+	                	 author:"admin",
+	                	 date:2,
+	                	 message:"qsdfh f sqldf q"   
+	                   },
+	                   {
+	                	 author:"team",
+	                	 date:2,
+	                	 message:"qsdfh QG QFD qg dfg sfd g sdf g sfg s g sf g f gs df gs dfg s dfg sdf g q fgs f gs dfg  GD SFG SDF s hs dh sd hs gfh sfgh f sqldf q"	                   },
+	                   {
+	                	 author:"admin",
+	                	 date:2,
+	                	 message:"qsdfh f sqldf q"	                   }	                   
+	                   
+	                   ];
+	  
+	  this.create = function(){
+		  
+	  }
+	  
   });
